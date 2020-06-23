a0abf114ee918185f4bf2d16640fe68fafddd4d6
Nina Herasymova <predvoditeldvoryanstva@gmail.com>
2020-06-23 22:29:17 +0300

    fix

--- index.php
+++ index.php
@@ -8,20 +8,4 @@ require_once 'vendor/autoload.php';
 $loader = new FilesystemLoader('views');
 $twig = new Environment($loader);
 
-$cards = array(
-  array(
-    'title' => 'Blandit turpis cursus in hac habitasse platea dictumst',
-    'date' => '11.11.2011',
-    'author' => 'Consectetur Purus',
-    'image' => 'https://i.pinimg.com/originals/8d/70/aa/8d70aa58b9ce9fcf1deb6745b78fd588.jpg'
-  ),
-  array(
-    'title' => 'Blandit turpis cursus in hac habitasse platea dictumst',
-    'date' => '11.11.2011',
-    'author' => 'Consectetur Purus',
-    'image' => 'https://i.pinimg.com/originals/8d/70/aa/8d70aa58b9ce9fcf1deb6745b78fd588.jpg'
-  )
-);
-
-
 echo $twig->render('template.twig');
